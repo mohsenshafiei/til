7dfd48bb5217e055b1a4aa4665b48a7da5c78b67
Mohsen Shafiei <mohsen.shafieitafreshi@shopee.com>
2021-04-19 23:20:10 +0800

    feat: add createElement example.

--- react/create-element/index.jsx
+++ react/create-element/index.jsx
@@ -1,19 +1,30 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
 // const element = React.createElement(type, [props], [...children]);
 
+const ref = React.createRef();
 const container = React.createElement(
-  'div',
-  { className: 'wrapper'}, 
-  React.createElement('h1', {}, 'My Header'), 
-  React.createElement('ul', {},
-    [
-      React.createElement('li', { className: 'e1'}, 'item 1'),
-      React.createElement('li', { className: 'e1'}, 'item 2')
-      React.createElement('li', { className: 'e1'}, 'item 3')
-    ]
-  )
+  "div",
+  { className: "wrapper", ref: ref },
+  React.createElement("h1", {}, "My Header"),
+  React.createElement("ul", {}, [
+    React.createElement("li", { className: "e1" }, "item 1"),
+    React.createElement("li", { className: "e1" }, "item 2"),
+    React.createElement("li", { className: "e1" }, "item 3"),
+  ])
 );
 
-ReactDOM.render(container, document.getElementById("app"))
+// ReactDOM.render(container, document.getElementById("app"));
+
+console.log(container);
+
+// {
+//   '$$typeof': Symbol(react.element),
+//   type: 'div',
+//   key: null,
+//   ref: { current: null},
+//   props: { className: 'wrapper', children: [ [Object], [Object] ] },
+//   _owner: null,
+//   _store: {}
+// }
