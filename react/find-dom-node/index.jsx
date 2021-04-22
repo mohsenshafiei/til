879d13c334008b6f25944bdac246e0ba2be04757
Mohsen Shafiei <mohsen.shafieitafreshi@shopee.com>
2021-04-22 13:05:38 +0800

    feat: add comment find dom node.

---
+++ react/find-dom-node/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    ReactDOM.findDOMNode(this.refs.myinput).focus();
+  }
+
+  render() {
+    return (
+      <>
+        <input type="text" ref="myinput" />
+        <input
+          type="button"
+          value="Click to focus input"
+          onClick={this.handleClick}
+        />
+      </>
+    );
+  }
+}
+
+export default MyComponent;
+
+//  It has been deprecated in StrictMode.
+
+// When a component renders to null or false,
+// findDOMNode returns null. When a component renders
+// to a string, findDOMNode returns a text DOM node
+// containing that value.
