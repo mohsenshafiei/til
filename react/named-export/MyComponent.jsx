15cf73c32d859c75a8daa0e1cb1b658c0a3c9e01
Mohsen Shafiei <mohsen.shafieitafreshi@shopee.com>
2021-04-24 12:35:52 +0800

    feat: add named-export.

---
+++ react/named-export/MyComponent.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const MyComponent = () => {
+  return <div>My Component</div>;
+};
---
+++ react/named-export/index.jsx
@@ -0,0 +1,3 @@
+// Currently react lazy only supports default export. Thus this is one approach.
+
+export { MyComponent as default } from "./MyComponents.js";
