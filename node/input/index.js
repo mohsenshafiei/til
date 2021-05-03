d54b0a6fb144cf5e27403b18cf111c856a5e8e5d
Mohsen Shafiei <mohsen.shafieitafreshi@shopee.com>
2021-05-03 14:42:35 +0800

    feat: add node input.

---
+++ node/input/index.js
@@ -0,0 +1,8 @@
+const inputs = process.argv;
+console.log(inputs)
+
+// node index.js hello
+
+// The first argument, 0, is the process.execPath, which is the path of the node executable.
+// The second argument, 1, is the path of the JavaScript file that is being run.
+// The next argument(s) are the command-line arguments, if any have been passed. In our case, this will be a simple Hello.
