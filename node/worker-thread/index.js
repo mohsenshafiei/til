10326dc64735cb7f3a036e7fa9e7a4eaede65918
Mohsen Shafiei <mohsen.shafieitafreshi@shopee.com>
2021-05-04 23:37:40 +0800

    feat: add worker thread.

---
+++ node/worker-thread/index.js
@@ -0,0 +1,36 @@
+const {
+  Worker, isMainThread, parentPort, workerData, MessageChannel, receiveMessageOnPort 
+} = require('worker_threads');
+
+if (isMainThread) {
+    module.exports = function parseJSAsync(script) {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(__filename, {
+        workerData: script
+      });
+      worker.on('message', resolve);
+      worker.on('error', reject);
+      worker.on('exit', (code) => {
+        if (code !== 0)
+          reject(new Error(`Worker stopped with exit code ${code}`));
+      });
+    });
+  };
+} else {
+  const { parse } = require('some-js-parsing-library');
+  const script = workerData;
+  parentPort.postMessage(parse(script));
+}
+
+const { port1, port2 } = new MessageChannel();
+port1.postMessage({ hello: 'world' });
+
+console.log(receiveMessageOnPort(port2));
+// Prints: { message: { hello: 'world' } }
+console.log(receiveMessageOnPort(port2));
+// Prints: undefined
+
+
+// worker.resourceLimits
+// worker.SHARE_ENV
+// worker.setEnvironmentData(key[, value])
