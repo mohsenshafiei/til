de46bb026c9145182a75ba0b5f173187367cbfd5
Mohsen Shafiei <mohsen.shafieitafreshi@shopee.com>
2021-05-04 16:52:18 +0800

    feat: add node globals.

---
+++ node/globals/index.js
@@ -0,0 +1,7 @@
+// console
+// setTimeout, setInterval, setImmediate
+// process
+
+// There is a difference in what is considered the global scope in Node.js
+// and browsers. In a browser, the top-level scope is the global scope, whereas,
+// in Node.js, the top-level scope is not the global scope; it is local to each module.
