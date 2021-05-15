98e24e38bf8eb4dc02747807f1c593f550164f3a
Mohsen Shafiei <mohsen.shafieitafreshi@shopee.com>
2021-05-15 23:28:36 +0800

    feat: add two more.

---
+++ leet-code/easy/4sum/index.js
@@ -0,0 +1,4 @@
+const threeSum = (arr, target) => {
+  let 
+
+}
---
+++ leet-code/easy/spiral-matrix/index.js
@@ -0,0 +1,8 @@
+const spiralMatrix = (matrix) => {
+  let k = 0;
+  for (let i = 0; i< matrix.length; i++) {
+    matrix[i][k];
+  }
+  for (let i)
+
+}
